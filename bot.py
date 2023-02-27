b4a993bbec9bfa6a3a324e2a48398266d9085933
Daniel Jeremiah <121419973+daniyyell-dev@users.noreply.github.com>
2023-02-27 07:53:38 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,7 +1,7 @@
 import requests
 from datetime import datetime, timedelta
 import time
-import telegram
+import telebot
 import logging
 
 # Set up logging
@@ -13,7 +13,7 @@ api_key = 'YOUR_API_KEY'
 # Set up the Telegram bot credentials
 bot_token = 'YOUR_BOT_TOKEN'
 chat_id = 'YOUR_CHAT_ID'
-bot = telegram.Bot(token=bot_token)
+bot = telebot.TeleBot(bot_token)
 
 try:
     # Set up the start and end times for the top 20 submissions
@@ -21,8 +21,7 @@ try:
     start_time = end_time - timedelta(days=1)
 
     # Get the top 20 malicious files for the specified time period from the Hybrid-Analysis API
-    response = requests.get(f'https://www.hybrid-analysis.com/api/v2/latest-submissions?start={start_time}&end={end_time}&sort=score&order=desc&size=20',
-                            headers={'api-key': api_key})
+    response = requests.get(f'https://www.hybrid-analysis.com/api/v2/latest-submissions?start={start_time}&end={end_time}&sort=score&order=desc&size=20', headers={'api-key': api_key})
     response.raise_for_status()
     top_20 = []
     for submission in response.json()['data']:
@@ -32,9 +31,9 @@ try:
 
     # Send the top 20 malicious files to the Telegram bot
     message = f'Top 20 Malicious Files for {start_time.date()}:\n\n' + '\n'.join(top_20)
-    bot.send_message(chat_id=chat_id, text=message)
+    bot.send_message(chat_id, message)
 
 except Exception as e:
     # Handle any errors that occur during the scraping or sending process
     logging.error(f'Error occurred: {e}')
-    bot.send_message(chat_id=chat_id, text=f'Error occurred while fetching top 20 malicious files: {e}')
+    bot.send_message(chat_id, f'Error occurred while fetching top 20 malicious files: {e}')
